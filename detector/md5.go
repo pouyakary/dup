993e5bbabdfacdcfac4c6c08dff68126ff58fe12
Pouya Kary ✨ <pouya@kary.us>
2022-03-19 21:17:56 +0330

    computes the md5 of a file

---
+++ detector/md5.go
@@ -0,0 +1,25 @@
+package detector
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ComputeFileMD5(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	md5 := fmt.Sprintf("%x", hash.Sum(nil))
+
+	return md5, nil
+}
