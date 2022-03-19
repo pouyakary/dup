311b37b7fe15b2828d8b00318b4cd22fb9c35061
Pouya Kary ✨ <pouya@kary.us>
2022-03-19 22:35:58 +0330

    fixed a hidden tap problem

--- command/help.go
+++ command/help.go
@@ -6,7 +6,7 @@ func DisplayHelp() {
 	fmt.Println("dup: a tool to find duplicate files, by dragon's lover")
 	fmt.Println("https://github.com/pouyakary/dup - kary@gnu.org\n")
 	fmt.Println("usage: dup [options] <directory>")
-	fmt.Println("  -h, help		displays this help message")
+	fmt.Println("  -h, help     displays this help message")
 	fmt.Println("  -q, quite    stops the software from displaying the results")
 	fmt.Println("  -r, remove   removes the duplicates from the directory")
 	fmt.Println("  -e, exact    compares the files exactly, without normalization")
